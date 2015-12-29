3cdd6d0e09b89ec4a748d686be5536d1fe9d7d2c
Brandon Graves <entaris@gmail.com>
2015-12-29 15:12:02 -0800

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Desc:         Deploy's profile information to the system for the user running it.
+######################################
+# AUTH:         Brandon M. Graves
+# Created:      29DEC2015
+# Last_Edit:    29DEC2015
+#######################################
+
+
+# First bashrc ############
+if [ -f ~/.bashrc ]; then
+        rm -f ~/.bashrc
+fi
+
+ln -s ${PWD}/bashrc ~/.bashrc
+##############################
+
+# Next Vim Related Things ###
+
+if [ -f ~/.vimrc ]; then
+        rm -f ~/.vimrc
+fi
+
+ln -s ${PWD}/vimrc ~/.vimrc
+
+if [ -f ~/.vim ]; then
+        ln -s ${PWD}/color ~/.vim/
+else
+        mkdir ~/.vim
+        ln -s ${PWD}/color ~/.vim/
+fi
