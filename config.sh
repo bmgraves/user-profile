e61a5093dfa635e2d5a9c1d42c2b4e69b2ceea51
bmgraves <entaris@gmail.com>
2015-12-31 13:37:20 -0800

    Config update

--- config.sh
+++ config.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-# Desc:         Deploy's profile information to the system for the user running it.
+# Desc: Deploy's profile information to the system for the user running it.
 ######################################
 # AUTH:         Brandon M. Graves
 # Created:      29DEC2015
-# Last_Edit:    29DEC2015
+# Last_Edit:    31DEC2015
 #######################################
 
 
 # First bashrc ############
 if [ -f ~/.bashrc ]; then
         rm -f ~/.bashrc
+	ln -s ${PWD}/bashrc ~/.bashrc
+else
+	ln -s ${PWD}/bashrc ~/.bashrc
 fi
 
-ln -s ${PWD}/bashrc ~/.bashrc
 ##############################
 
 # Next Vim Related Things ###
 
 if [ -f ~/.vimrc ]; then
-        rm -f ~/.vimrc
+	rm -f ~/.vimrc
+	ln -s ${PWD}/vimrc ~/.vimrc
+else
+	ln -s ${PWD}/vimrc ~/.vimrc
 fi
 
-ln -s ${PWD}/vimrc ~/.vimrc
-
-if [ -f ~/.vim ]; then
-        ln -s ${PWD}/colors ~/.vim/
+if [ -d ~/.vim ]; then
+	if [ -f ~/.vim/colors ]; then
+		rm -f ~/.vim/colors
+        	ln -s ${PWD}/colors ~/.vim/
+	else
+        	ln -s ${PWD}/colors ~/.vim/
+	fi
 else
         mkdir ~/.vim
         ln -s ${PWD}/colors ~/.vim/
